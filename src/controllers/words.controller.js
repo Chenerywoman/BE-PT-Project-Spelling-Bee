47d2a981f41fdccdea9390fb1f64396833fa9d2a
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-08 18:32:51 +0100

    adding getPrefixes function

--- src/controllers/words.controller.js
+++ src/controllers/words.controller.js
@@ -1,10 +1,22 @@
-// getWords, getSuffixes, getPrefixes, getMedials, getHomophones, getFree, getMixed
+// getSuffixes, getMedials, getHomophones, getFree, getMixed
 
-const {findAllWords, findSuffixes, findPrefixes, findMedials, findHomophones, findFree, findMixed} = require('../queries/words.queries');
+const {findAllWords, findPrefixes, findSuffixes,findMedials, findHomophones, findFree, findMixed} = require('../queries/words.queries');
 
 exports.getWords = (req, res, next) => {
     return findAllWords()
     .then(words => res.status(200).send({words}))
     .catch(() => next({status: 500, controller: 'words'}));
 };
+
+exports.getPrefixes = (req, res, next) => {
+    const {prefix} = req.query;
+    return findPrefixes(prefix)
+    .then(words => {
+        res.status(200).send({words});
+    })
+    .catch(err => {
+        if (err.status === 404) return next(err);
+        else return next({ status: 500, controller: 'words' });
+      });
+}
 
