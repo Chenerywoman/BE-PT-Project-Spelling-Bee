f37e2d9cd3adbac586a7758972eea8f4affa6231
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-08 15:46:31 +0100

    creating testData folder & json files

---
+++ seed/data/testData/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    testWords: require('./testWords'),
+    testMedials: require('./testMedials'),
+    testSuffixes: require('./testSuffixes'),
+    testPrefixes: require('./testPrefixes')
+};
