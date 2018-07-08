141cd28d98762bbb6610025c8c047e8081e0754b
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-08 15:58:40 +0100

    renaming export to app

---
+++ server.js
@@ -0,0 +1,31 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+const mongoose = require('mongoose');
+const express = require('express');
+// const cors = require('cors');
+const app = express();
+let url = process.env.NODE_ENV === 'production' ? process.env.MONGO_URL : require('./config/index');
+const {json} = require('body-parser');
+const apiRouter = require('./src/routers/api.router.js');
+
+mongoose.connect(url, { useNewUrlParser: true });
+
+app.use(json());
+
+// app.use(cors());
+
+app.get('/', function(req, res) {
+    return res.status(200).send('please see /api for routes');
+});
+
+app.use(express.static('public'));
+
+app.use('/api', apiRouter);
+
+app.use((err, req, res, next) => {
+res.status !== 500
+? res.status(err.status).send({error: err.message})
+: res.status(500).send({error: `server error in ${err.controller}`});
+});
+
+module.exports = app;
+
