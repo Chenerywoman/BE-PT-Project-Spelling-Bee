40e2f1f54e68ad186d0afd61e54e05670f4102fd
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-08 12:25:25 +0100

    moving folders

--- data/seed.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Word = require('../models/words');
-
-exports.seed = (wordsData) => {
-
-    return mongoose.connection.dropDatabase()
-        .then(() => Word.insertMany(wordsData));
-};
