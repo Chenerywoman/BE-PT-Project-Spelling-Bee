55386bd8eae5a12a683c23a9c517fbf30ca57531
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-10 10:56:42 +0100

    changing reference to words.model.js

--- src/queries/words.queries.js
+++ src/queries/words.queries.js
@@ -1,4 +1,4 @@
-const Word = require('../models/words');
+const Word = require('../models/words.model');
 
 // findMixed
 
@@ -13,3 +13,4 @@ exports.findMedials = (medial) => Word.find({'categories.medials': medial}).lean
 exports.findHomophones = (homophone) => Word.find({'categories.homophones': homophone}).lean();
 
 exports.findFree = () => Word.find({$and:[ {'categories.prefixes': {$size: 0}}, {'categories.suffixes': {$size: 0}}, {'categories.medials': {$size: 0}}, {'categories.homophones': {$size: 0}}]}).lean();
+
