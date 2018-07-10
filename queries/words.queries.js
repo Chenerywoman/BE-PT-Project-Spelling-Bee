3660dd192c98fa35daf3c993781c57e3f7d98009
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-10 12:42:36 +0100

    adding select to remove _v from returned objects

--- queries/words.queries.js
+++ queries/words.queries.js
@@ -2,15 +2,15 @@ const Word = require('../models/words.model');
 
 // findMixed
 
-exports.findAllWords = () => Word.find().lean();
+exports.findAllWords = () => Word.find().select('-__v').lean();
 
-exports.findPrefixes = (prefix) => Word.find({'categories.prefixes': prefix}).lean();
+exports.findPrefixes = (prefix) => Word.find({'categories.prefixes': prefix}).select('-__v').lean();
 
-exports.findSuffixes = (suffix) => Word.find({'categories.suffixes': suffix}).lean();
+exports.findSuffixes = (suffix) => Word.find({'categories.suffixes': suffix}).select('-__v').lean();
 
-exports.findMedials = (medial) => Word.find({'categories.medials': medial}).lean();
+exports.findMedials = (medial) => Word.find({'categories.medials': medial}).select('-__v').lean();
 
-exports.findHomophones = (homophone) => Word.find({'categories.homophones': homophone}).lean();
+exports.findHomophones = (homophone) => Word.find({'categories.homophones': homophone}).select('-__v').lean();
 
-exports.findFree = () => Word.find({$and:[ {'categories.prefixes': {$size: 0}}, {'categories.suffixes': {$size: 0}}, {'categories.medials': {$size: 0}}, {'categories.homophones': {$size: 0}}]}).lean();
+exports.findFree = () => Word.find({$and:[ {'categories.prefixes': {$size: 0}}, {'categories.suffixes': {$size: 0}}, {'categories.medials': {$size: 0}}, {'categories.homophones': {$size: 0}}]}).select('-__v').lean();
 
