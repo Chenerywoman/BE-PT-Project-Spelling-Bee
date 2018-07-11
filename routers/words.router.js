ef570f94aafc24a6979a62c058ca2fe6dc01f3d2
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-11 13:24:49 +0100

    adding delete to route

--- routers/words.router.js
+++ routers/words.router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {getWords, getFree, postNewWord, deleteWord} = require('../controllers/words.controller');
+const {getWords, getFree, postNewWord, removeWord} = require('../controllers/words.controller');
 
 router.route('/')
 .get(getWords)
-.post(postNewWord);
-// .delete(deleteWord);
+.post(postNewWord)
+.delete(removeWord);
 
 router.get('/freestyle', getFree);
 
