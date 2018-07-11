c7ece17671d921de702c31155c51622adb12d409
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-11 13:24:15 +0100

    adding removeWord function

--- controllers/words.controller.js
+++ controllers/words.controller.js
@@ -1,4 +1,4 @@
-const { findAllWords, findFree, createWord, checkforDuplicate } = require('../queries/words.queries');
+const { findAllWords, findFree, createWord, checkforWord, deleteWord } = require('../queries/words.queries');
 
 exports.getWords = (req, res, next) => {
     return findAllWords()
@@ -15,29 +15,42 @@ exports.getFree = (req, res, next) => {
 exports.postNewWord = (req, res, next) => {
     const objectKeys = ['word', 'categories'];
     const body = Object.keys(req.body);
-    objectKeys.forEach((elem, ind) => { if (body[ind] !== elem) throw { status: 400, message: `${body[ind]} is an invalid key`}; });
+    objectKeys.forEach((elem, ind) => { if (body[ind] !== elem) throw { status: 400, message: `${body[ind]} is an invalid key` }; });
     const categoryKeys = ['prefixes', 'suffixes', 'medials', 'homophones'];
     const categories = Object.keys(req.body.categories);
     categoryKeys.forEach((elem, ind) => { if (categories[ind] !== elem) throw { status: 400, message: `${categories[ind]} is an invalid key` }; });
 
-    return checkforDuplicate(req.body.word)
-    .then(word => {
-        if (word.length) {throw { status: 400, message: `${word[0].word} already exists in the database`};}
-        else {return createWord(req.body.word, req.body.categories.prefixes, req.body.categories.suffixes, req.body.categories.medials, req.body.categories.homophones);}
-    })
+    return checkforWord(req.body.word)
         .then(word => {
-            return res.status(201).send({ new_word: word });
+            if (word.length) { throw { status: 400, message: `${word[0].word} already exists in the database` }; }
+            else { return createWord(req.body.word, req.body.categories.prefixes, req.body.categories.suffixes, req.body.categories.medials, req.body.categories.homophones); }
         })
+        .then(word => res.status(201).send({ new_word: word }))
         .catch((err) => {
-            if (err.name === 'MongoError' && err.code === 11000) {return next({status: 400, message: `${req.body.word} already exists in the database`})}
+            if (err.name === 'MongoError' && err.code === 11000) { return next({ status: 400, message: `${req.body.word} already exists in the database` }) }
             else if (err.status === 400) return next(err);
             else return next({ status: 500, controller: 'words' });
         });
 };
 
-// exports.deleteWord = (req, res, next) => {
-//     return 
-    
-
-// }
+exports.removeWord = (req, res, next) => {
+    console.log('req.body', req.body);
+    const word = req.body.word;
+    return checkforWord(word)
+    .then(checkedWord => {
+        console.log('checkedWord', checkedWord);
+        console.log('word', checkedWord[0].word);
+        if (checkedWord.length && checkedWord[0].word === word) return deleteWord(word);
+        else { throw { status: 404, message: `${word} does not exist in the database`}; }
+    })
+    .then(deleteResponse => {
+        console.log('res after delete', deleteResponse);
+        if (deleteResponse.n === 1 && deleteResponse.ok === 1) return res.status(200).send({delete_message:`${word} successfully deleted`});
+    })
+    .catch(err => {
+        console.log('err', err);
+        if (err.status === 404) return next(err);
+        else return next ({status: 500, controller: 'words'});
+    });
+};
 
