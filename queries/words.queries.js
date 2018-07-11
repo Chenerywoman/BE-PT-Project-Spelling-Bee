2bfe206a20962c32188a11fdc723217ba8227d65
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-11 13:24:28 +0100

    adding deleteWord function

--- queries/words.queries.js
+++ queries/words.queries.js
@@ -4,8 +4,8 @@ exports.findAllWords = () => Word.find().select('-__v').lean();
 
 exports.findFree = () => Word.find({$and:[ {'categories.prefixes': {$size: 0}}, {'categories.suffixes': {$size: 0}}, {'categories.medials': {$size: 0}}, {'categories.homophones': {$size: 0}}]}).select('-__v').lean();
 
-exports.checkforDuplicate = (word) => Word.find({word});
+exports.checkforWord = (word) => Word.find({word});
  
 exports.createWord = (word, prefixes, suffixes, medials, homophones) => Word.create({word, categories: {prefixes: prefixes, suffixes: suffixes, medials: medials, homophones :homophones}});
  
-exports.deleteWord = (word) => Word.deleteOne({word}).lean()
+exports.deleteWord = (word) => Word.deleteOne({word}).lean();
