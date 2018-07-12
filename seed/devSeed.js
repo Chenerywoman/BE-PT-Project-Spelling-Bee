bf609805177e4e815e654212e3c77c8786703219
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-12 12:03:46 +0100

    adding description json files to files passed to  the dataMaker function

--- seed/devSeed.js
+++ seed/devSeed.js
@@ -1,11 +1,11 @@
-const {words, prefixes, suffixes, medials, homophones} = require('./data/devData/index');
+const {words, prefixes, suffixes, medials, homophones, prefixesDescription, suffixesDescription, medialsDescription, homophonesDescription } = require('./data/devData/index');
 const {wordsMaker, categoriesMaker} = require('./dataMakers');
 const mongoose = require('mongoose');
 const url = require('../config/index');
 const {seed} = require('./seed');
 
 const wordData = wordsMaker(words, prefixes, suffixes, medials);
-const categoriesData = categoriesMaker(prefixes, suffixes, medials, homophones);
+const categoriesData = categoriesMaker(prefixes, suffixes, medials, homophones, prefixesDescription, suffixesDescription, medialsDescription, homophonesDescription);
 
 mongoose.connect(url, { useNewUrlParser: true })
 .then(() => seed(wordData, categoriesData))
