f6879f77e47489f4eeea340a1c583bd32cc3a3ad
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-14 18:15:52 +0100

    adding function to find partials by category

--- queries/partials.queries.js
+++ queries/partials.queries.js
@@ -1,9 +1,10 @@
 const Partial = require('../models/partials.model');
 
 exports.findPostedPartials = (partials) => {
-        return Promise.all ( partials.map(partial => {
+        return Promise.all (partials.map(partial => {
            return Partial.find({partial});
         }));
 
 };
 
+exports.findPartialsByCategory = (categoryId) =>  Partial.find({category: categoryId}).select('-__v').lean();
