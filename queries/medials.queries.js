8dd46ccd1e3e11ea7726163ea6b052909bf20afd
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-14 20:56:25 +0100

    adding populate to years & partials

--- queries/medials.queries.js
+++ queries/medials.queries.js
@@ -1,4 +1,5 @@
 const Word = require('../models/words.model');
 
-exports.findMedials = (medialId) => Word.find({partials: medialId}).select('-__v').populate({path: 'partials', select: 'letters'}).populate({path: 'years', select: 'year'}).lean();
+exports.findMedials = (medialId) => Word.find({partials: medialId}).select('-__v').populate({path: 'partials', select: 'letters', populate:{path: 'categories', select: 'name'}}).populate({path: 'years', select: 'year'}).lean();
+
 
