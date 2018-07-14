4149ec0269f12ed2c9456ccdafb11a464c9e4243
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-14 20:54:42 +0100

    amending partials & types with outer array in order to allow populate

--- models/words.model.js
+++ models/words.model.js
@@ -14,14 +14,9 @@ lowercase: true,
 // type: [Schema.Types.ObjectId],
 // ref: 'partials'
 // }
-partials: [{ type: Schema.Types.ObjectId, ref: 'partials' }]
-,
+partials: [{ type: Schema.Types.ObjectId, ref: 'partials' }],
 
-years: {
-    type: [Schema.Types.ObjectId],
-    ref: 'years',
-    required: true
-}
+years: [{type: [Schema.Types.ObjectId], ref: 'years', required: true}]
 
 });
 
