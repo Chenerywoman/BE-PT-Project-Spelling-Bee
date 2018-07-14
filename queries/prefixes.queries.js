34a62d2d212823dbd08aca86ca15531abe89c4ce
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-14 21:17:39 +0100

    amending findPrefixes to search for partial id matching one passed & populating

--- queries/prefixes.queries.js
+++ queries/prefixes.queries.js
@@ -1,3 +1,7 @@
 const Word = require('../models/words.model');
 
-exports.findPrefixes = (prefix) => Word.find({'categories.prefixes': prefix}).select('-__v').lean();
+exports.findPrefixes = (prefixId) => Word.find({partials: prefixId}).select('-__v').populate({path: 'partials', select: 'letters', populate:{path: 'categories', select: 'name'}}).populate({path: 'years', select: 'year'}).lean();
+
+
+
+
