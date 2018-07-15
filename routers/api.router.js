5cb1a9e62b66e7d746d0cab1612266467f84fd49
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 10:37:42 +0100

    adding a years route

--- routers/api.router.js
+++ routers/api.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const yearsRouter = require('./years.router');
 const wordsRouter = require('./words.router');
 const prefixesRouter = require('./prefixes.router');
 const suffixesRouter = require('./suffixes.router');
@@ -8,6 +9,8 @@ const homophonesRouter = require('./homophones.router');
 const path = require('path');
 
 router.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '..', 'public/index.html')));
+
+router.use('/years', yearsRouter);
 
 router.use('/words', wordsRouter);
 
