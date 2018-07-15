24bbb05e68af0b3c7df4a17ba866c3189d56088a
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 15:36:07 +0100

    removing route to /freestyle which now has separate route

--- routers/words.router.js
+++ routers/words.router.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {getWords, getFree, postNewWord, removeWord} = require('../controllers/words.controller');
+const {getWords, postNewWord, removeWord} = require('../controllers/words.controller');
 
 router.route('/')
 .get(getWords)
 .post(postNewWord)
 .delete(removeWord);
-
-router.get('/freestyle', getFree);
 
 module.exports = router;
