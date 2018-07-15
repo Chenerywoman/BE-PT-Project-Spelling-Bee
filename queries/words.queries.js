7a40635291b0d71e81ca8cb502b96e0fa619e8a3
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 10:48:58 +0100

    changing query for findAllWords to only return certain fields in populate

--- queries/words.queries.js
+++ queries/words.queries.js
@@ -1,7 +1,10 @@
 const Word = require('../models/words.model');
+
 // this query just returns selected fields in populate
-// exports.findAllWords = () => Word.find().select('-__v').populate({path: 'partials', select: 'letters', populate:{path: 'categories', select: 'name'}}).populate({path:'years', select: 'year'}).lean();
-exports.findAllWords = () => Word.find().select('-__v').populate({path: 'partials', populate:{path: 'categories'}}).populate({path:'years', select: 'year'}).lean();
+exports.findAllWords = () => Word.find().select('-__v').populate({path: 'partials', select: 'letters', populate:{path: 'categories', select: 'name'}}).populate({path:'years', select: 'year'}).lean();
+
+// this query returns all fields in the populated collections
+// exports.findAllWords = () => Word.find().select('-__v').populate({path: 'partials', populate:{path: 'categories'}}).populate({path:'years', select: 'year'}).lean();
 
 exports.findFree = () => Word.find({partials: []}).select('-__v').populate({path: 'years', select: 'year'}).lean();
 
