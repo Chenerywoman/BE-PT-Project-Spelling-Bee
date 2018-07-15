3fd53e5642691d2629dc209627d4e0b20c457b09
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 17:08:54 +0100

    adding error-handling to getCategories to deal with query string for non-existent year

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,18 +1,24 @@
 const { findAllCategories, findCategoriesByYear, findCategory } = require('../queries/categories.queries');
-const {findYear} = require('../queries/years.queries')
+const { findYear } = require('../queries/years.queries')
 
 exports.getCategories = (req, res, next) => {
     if (req.query.year) {
-        const year = req.query.year;
-        return findYear(year)
-        .then(([year]) => {
-            const yearId = year._id;
-            return findCategoriesByYear(yearId);
-        })
+        const yearRequested = req.query.year;
+        return findYear(yearRequested)
+            .then(([year]) => {
+                if (!year) throw { status: 404, message: `year ${yearRequested} not found` };
+                else {
+                    const yearId = year._id;
+                    return findCategoriesByYear(yearId);
+                }
+            })
             .then(categories => {
                 res.status(200).send({ categories })
             })
-            .catch(() => next({ status: 500, controller: 'categories' }));
+            .catch(err => {
+                if (err.status === 400 || err.status === 404) return next(err);
+                else return next({ status: 500, controller: 'categories' });
+            });
     } else {
         return findAllCategories()
             .then(categories => res.status(200).send({ categories }))
