57d361338604b5c1c3f372e59485a75431bb7629
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 16:52:20 +0100

    adding if clause to getCategories function to allow a query string to query for categories by year

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,9 +1,23 @@
-const {findAllCategories, findCategory} = require('../queries/categories.queries')
+const { findAllCategories, findCategoriesByYear, findCategory } = require('../queries/categories.queries');
+const {findYear} = require('../queries/years.queries')
 
 exports.getCategories = (req, res, next) => {
-    return findAllCategories()
-    .then(categories => res.status(200).send({categories}))
-    .catch(() => next({status: 500, controller: 'categories'}));
+    if (req.query.year) {
+        const year = req.query.year;
+        return findYear(year)
+        .then(([year]) => {
+            const yearId = year._id;
+            return findCategoriesByYear(yearId);
+        })
+            .then(categories => {
+                res.status(200).send({ categories })
+            })
+            .catch(() => next({ status: 500, controller: 'categories' }));
+    } else {
+        return findAllCategories()
+            .then(categories => res.status(200).send({ categories }))
+            .catch(() => next({ status: 500, controller: 'categories' }));
+    }
 };
 
 exports.getCategory = (req, res, next) => {
