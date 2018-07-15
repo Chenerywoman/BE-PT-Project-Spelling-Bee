4bafff678e5933e918689d052f7b36e8d76cf58f
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-15 11:42:33 +0100

    correcting findPostedParticles function to search in 'letters' array

--- queries/partials.queries.js
+++ queries/partials.queries.js
@@ -3,10 +3,9 @@ const Partial = require('../models/partials.model');
 exports.findPartial = (partial) => Partial.find({letters: partial});
 
 exports.findPostedPartials = (partials) => {
-        return Promise.all (partials.map(partial => {
-           return Partial.find({partial});
+       return Promise.all (partials.map(partial => {
+           return Partial.find({letters: partial});
         }));
-
 };
 
 exports.findPartialsByCategory = (categoryId) =>  Partial.find({categories: categoryId}).select('-__v').populate({path: 'categories', select: 'name'}).populate({path: 'years', select: 'year'}).lean();
