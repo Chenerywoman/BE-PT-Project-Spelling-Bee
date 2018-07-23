40de1db9b5f491543818bde5a80bd617c1758cfb
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-23 14:02:55 +0100

    adding /* eslint-disable no-console */ to file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-console */
+
 const app = require('./server');
 
 let PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : 3000;
 
 app.listen(PORT, function (err) {
 if (err) return console.log(err);
-console.log(`Server listening on port ${PORT}`)
+console.log(`Server listening on port ${PORT}`);
 });
