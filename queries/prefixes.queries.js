b91cb77d6b869bc05d69b5fd22709e3f723587f1
Chenerywoman <rachelmchenery@hotmail.com>
2018-07-23 14:07:07 +0100

    fixing linting - space at end of file

--- queries/prefixes.queries.js
+++ queries/prefixes.queries.js
@@ -2,6 +2,3 @@ const Word = require('../models/words.model');
 
 exports.findPrefixes = (prefixId) => Word.find({partials: prefixId}).select('-__v').populate({path: 'partials', select: 'letters', populate:{path: 'categories', select: 'name'}}).populate({path: 'years', select: 'year'}).lean();
 
-
-
-
